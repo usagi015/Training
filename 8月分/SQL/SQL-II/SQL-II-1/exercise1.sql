1e0d4facc7b31fb7f26caa0cdd159269abbdc001
Phuong <phuongntv015@gmail.com>
2023-09-01 04:19:31 +0900

    Add SQL-II source code

---
+++ 8月分/SQL/SQL-II/SQL-II-1/exercise1.sql
@@ -0,0 +1,4 @@
+-- 実行ボタンを押し、重複を除かない状態でcharacter_nameカラムのデータを取得してみましょう。
+
+SELECT character_name
+FROM purchases;
---
+++ 8月分/SQL/SQL-II/SQL-II-1/exercise3.sql
@@ -0,0 +1,4 @@
+-- 重複するデータを除いたnameカラムのデータを取得してください
+
+SELECT DISTINCT(name)
+FROM purchases;
