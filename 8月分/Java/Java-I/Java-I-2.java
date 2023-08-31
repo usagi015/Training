e06d232d82aa4aa2ee916018f6aaa82ab3286cb2
Phuong <phuongntv015@gmail.com>
2023-09-01 01:57:55 +0900

    Add Java-I source code

---
+++ 8月分/Java/Java-I/Java-I-2.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+    
+    System.out.println("こんにちは、Java");
+    //この文章をコメントにしてください
+    
+    // 「こんにちは、Java」と出力してください
+    
+    
+  }
+}
+
