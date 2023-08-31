e0ddfe002a35459fcd3e602d99c700cf6e646fb3
Phuong <phuongntv015@gmail.com>
2023-09-01 03:45:23 +0900

    Add JavaScript-I source code

---
+++ 8月分/Javascript/JS-I/JS-I-18.js
@@ -0,0 +1,22 @@
+const n = 2;
+
+switch (n) {
+  case 1:
+    console.log("大吉です");
+    break;
+
+  // nの値が2のcaseを追加してください
+  case 2:
+    console.log("吉です");
+    break;
+    
+  case 3:
+    console.log("小吉です");
+    break;
+  
+  
+  // nの値が3のcaseを追加してください
+  
+  
+  
+}
