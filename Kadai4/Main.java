947c74e7526623f9985c27407a639429e55988d1
Phuong <phuongntv015@gmail.com>
2024-02-23 23:41:27 +0900

    Kadai4

---
+++ Kadai4/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int[][] table = new int[N][N];
+
+        for (int i = 0; i < N; i ++){
+            String input = scanner.nextLine();
+            String[] num = input.split(" ");
+            table[i][]
+        }
+    }
+}
